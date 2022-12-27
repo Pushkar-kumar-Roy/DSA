74f5d3d4d46ccc8ca7fab80d59b0e00a2d783d29
Pushkar-kumar-Roy <114002131+Pushkar-kumar-Roy@users.noreply.github.com>
2022-12-28 00:18:56 +0530

    Create First_last_position.c

---
+++ Assignment7.1/First_last_position.c
@@ -0,0 +1,39 @@
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        int loc1=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]<=target){
+                if(mid<loc1)
+                loc1=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid+1;
+            }
+        }
+        low=0;
+        high=nums.size()-1;
+        int loc2=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                if(loc2>mid)
+                loc2=mid;
+                high=mid-1;
+            }
+        }
+        vector<int>A={loc1,loc2};
+        return A;
+    }
+};

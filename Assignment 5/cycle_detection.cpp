ca80071ba608c4233c5a3848afc7af8e8da46bc5
Pushkar-kumar-Roy <114002131+Pushkar-kumar-Roy@users.noreply.github.com>
2023-02-07 15:05:55 +0530

    Update cycle_detection.cpp

--- Assignment 5/cycle_detection.cpp
+++ Assignment 5/cycle_detection.cpp
@@ -2,3 +2,87 @@ https://www.hackerrank.com/challenges/detect-whether-a-linked-list-contains-a-cy
 
 
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the has_cycle function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool has_cycle(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode*start1=head;
+    SinglyLinkedListNode*start2=head->next;
+    while(start1->next && start2->next && start2->next->next){
+        if(start1==start2)
+        return 1;
+    start1=start1->next;
+    start2=start2->next->next;
+    }
+    return 0;
+
+}
+
+int main()

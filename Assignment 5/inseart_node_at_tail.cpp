0ae619c1f5cc66175709339797a53bba713cfe3b
Pushkar-kumar-Roy <114002131+Pushkar-kumar-Roy@users.noreply.github.com>
2023-02-07 10:35:31 +0530

    Create inseart_node_at_tail.cpp

---
+++ Assignment 5/inseart_node_at_tail.cpp
@@ -0,0 +1,79 @@
+https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=true
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+        }
+
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+         SinglyLinkedListNode*start=head;
+         SinglyLinkedListNode*temp=new SinglyLinkedListNode(data);
+         temp->data=data;
+         temp->next=NULL;
+         if(start==NULL){
+             start=temp;
+             return start;
+         }
+         while(start->next!=NULL){
+             start=start->next;
+         }
+         start->next=temp;
+         return head;
+      
+}
+
+int main()
